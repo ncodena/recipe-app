25e7d2e34d684b8e6a60455a791e7cc2b4d0b2c6
Nuria <c.odena.nuria@gmail.com>
2020-05-14 19:38:42 +0200

    Passing Recipe Data with Property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../../recipes/recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg')
+    new Recipe('A test recipe', 'this is a test', 'https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg'),
+    new Recipe('A second recipe', 'this is a test', 'https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg')
   ];
 
   constructor() { }
